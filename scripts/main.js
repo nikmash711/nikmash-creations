345fc394f9075bf3838a7f412b508ec5fa57a800
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-13 00:13:17 -0800

    working on design

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,14 @@ $(document).ready(function(){
   checkWindowSize();
   
   navbarChangeOnScroll();
+
+  $(window).scroll(function() {  
+    navbarChangeOnScroll();
+  });
   
   $(window).resize(function() {
     checkWindowSize();
+    navbarChangeOnScroll();
   });
   
   // clickOutsideNavbar();  
@@ -16,70 +21,68 @@ function navbarChangeOnScroll(){
   // change the navbar when you scroll 
   let stickySidebar = $('.about').offset().top;
   stickySidebar= stickySidebar-95;
-  $(window).scroll(function() {  
-    if ($(window).scrollTop() > stickySidebar) {
-      $('.nav-link').css('color', '#e74c3c');
-      $('.nav-link').css('animation', 'font-color 30s infinite');
-      $('.nav-link').css('-webkit-animation', 'font-color 30s infinite');
+  if ($(window).scrollTop() > stickySidebar) {
+    $('.nav-link').css('color', '#e74c3c');
+    $('.nav-link').css('animation', 'font-color 30s infinite');
+    $('.nav-link').css('-webkit-animation', 'font-color 30s infinite');
+    $('.mobile .nav-link').css('color', 'white');
+    $('.mobile .nav-link').css('animation', 'none');
+    $('.mobile .nav-link').css('-webkit-animation', 'none');
       
-      // }          
-      $('.navbar-brand').css('color', '#e74c3c');
-      $('.navbar-brand').css('animation', 'font-color 30s infinite');
-      $('.navbar-brand').css('-webkit-animation', 'font-color 30s infinite');
-      $('.navbar').css('background-color', 'black');
-      $('.navbar').css('transition', ' background-color 1s');
-      $('.mobile .nav-link').css('color', 'white');
-      $('.mobile .nav-link').css('animation', 'none');
-      $('.mobile .nav-link').css('-webkit-animation', 'none');
-      // $('.navbar').css('border-color', '#e74c3c');
-      $('.navbar').css('transition', ' border-color 1s');
-      $('.navbar').css('animation', 'border-color 30s infinite');
-      $('.navbar').css('-webkit-animation', 'border-color 30s infinite');
-      $('#welcome_text').css('animation', 'none');
-      $('#welcome_text').css('-webkit-animation', 'none');
-      //  $("#arrow_down").css("animation", "none");
-      // $("#arrow_down").css("-webkit-animation", "none");
-      $('.custom-toggler.navbar-toggler ').css('border-color', '#e74c3c');
-      $('.custom-toggler.navbar-toggler ').css('animation', 'border-color 30s infinite');
-      $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'border-color 30s infinite');
-      $('.fa-bars').css('color', '#e74c3c');
-      $('.fa-bars').css('animation', 'font-color 30s infinite');
-      $('.fa-bars').css('-webkit-animation', 'font-color 30s infinite');
-      $('footer').css('animation', 'bg-color 30s infinite');
-      $('footer').css('-webkit-animation', 'bg-color 30s infinite');
+    $('.navbar-brand').css('color', '#e74c3c');
+    $('.navbar-brand').css('animation', 'font-color 30s infinite');
+    $('.navbar-brand').css('-webkit-animation', 'font-color 30s infinite');
+    $('.navbar').css('background-color', 'black');
+    $('.navbar').css('transition', ' background-color 1s');
+    // $('.navbar').css('border-color', '#e74c3c');
+    $('.navbar').css('transition', ' border-color 1s');
+    $('.navbar').css('animation', 'border-color 30s infinite');
+    $('.navbar').css('-webkit-animation', 'border-color 30s infinite');
+    $('#welcome_text').css('animation', 'none');
+    $('#welcome_text').css('-webkit-animation', 'none');
+    //  $("#arrow_down").css("animation", "none");
+    // $("#arrow_down").css("-webkit-animation", "none");
+    $('.custom-toggler.navbar-toggler ').css('border-color', '#e74c3c');
+    $('.custom-toggler.navbar-toggler ').css('animation', 'border-color 30s infinite');
+    $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'border-color 30s infinite');
+    $('.fa-bars').css('color', '#e74c3c');
+    $('.fa-bars').css('animation', 'font-color 30s infinite');
+    $('.fa-bars').css('-webkit-animation', 'font-color 30s infinite');
+    $('footer').css('animation', 'bg-color 30s infinite');
+    $('footer').css('-webkit-animation', 'bg-color 30s infinite');
       
-    }
-    else {
-      $('.nav-link').css('color', 'black');
-      $('.nav-link').css('animation', 'none');
-      $('.nav-link').css('-webkit-animation', 'none');
-      $('.navbar-brand').css('color', 'black');
-      $('.navbar-brand').css('animation', 'none');
-      $('.navbar-brand').css('-webkit-animation', 'none');
-      $('.navbar').css('transition', 'background-color 1s');
-      $('.navbar').css('background-color', '#e74c3c');
-      $('.navbar').css('animation', 'bg-color 30s infinite');
-      $('.navbar').css('-webkit-animation', 'bg-color 30s infinite');
-      // $('.navbar').css('border-color', 'transparent');
-      $('.navbar').css('transition', ' border-color 1s');
-      $('#welcome_text').css('animation', 'border-color 30s infinite');
-      $('#welcome_text').css('-webkit-animation', 'font-color 30s infinite');
-      // $("#arrow_down").css("animation", "font-color 30s infinite");
-      // $("#arrow_down").css("-webkit-animation", "font-color 30s infinite");
-      $('.custom-toggler.navbar-toggler ').css('border-color', 'black');
-      $('.custom-toggler.navbar-toggler ').css('animation', 'none');
-      $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'none');
-      $('.fa-bars').css('color', 'black');
-      $('.fa-bars').css('animation', 'none');
-      $('.fa-bars').css('-webkit-animation', 'none');
-      $('footer').css('animation', 'none');
-      $('footer').css('-webkit-animation', 'none');
-    }  
-  }); 
+  }
+  else {
+    $('.nav-link').css('color', 'black');
+    $('.nav-link').css('animation', 'none');
+    $('.nav-link').css('-webkit-animation', 'none');
+      
+    $('.navbar-brand').css('color', 'black');
+    $('.navbar-brand').css('animation', 'none');
+    $('.navbar-brand').css('-webkit-animation', 'none');
+    $('.navbar').css('transition', 'background-color 1s');
+    $('.navbar').css('background-color', '#e74c3c');
+    $('.navbar').css('animation', 'bg-color 30s infinite');
+    $('.navbar').css('-webkit-animation', 'bg-color 30s infinite');
+    // $('.navbar').css('border-color', 'transparent');
+    $('.navbar').css('transition', ' border-color 1s');
+    $('#welcome_text').css('animation', 'border-color 30s infinite');
+    $('#welcome_text').css('-webkit-animation', 'font-color 30s infinite');
+    // $("#arrow_down").css("animation", "font-color 30s infinite");
+    // $("#arrow_down").css("-webkit-animation", "font-color 30s infinite");
+    $('.custom-toggler.navbar-toggler ').css('border-color', 'black');
+    $('.custom-toggler.navbar-toggler ').css('animation', 'none');
+    $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'none');
+    $('.fa-bars').css('color', 'black');
+    $('.fa-bars').css('animation', 'none');
+    $('.fa-bars').css('-webkit-animation', 'none');
+    $('footer').css('animation', 'none');
+    $('footer').css('-webkit-animation', 'none');
+  }  
 }
 
 function checkWindowSize(){
-  if (window.innerWidth < 700){
+  if (window.innerWidth < 800){
     $('.collapse').css('display', 'none');
     $('.collapse').addClass('mobile');
     $('.brand1').fadeTo(1000,1);
