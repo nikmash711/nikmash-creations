98510d3759142896b3323d01f8a85552e5db167c
Nikkie Mashian <nikkiemashian@gmail.com>
2019-03-06 14:39:39 -0800

    js cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,48 @@
 'use strict'; 
 
+/*GLOBALS for CSS Manipulation*/
+const collapsible = $('.collapse');
+const navLink = $('.nav-link');
+const navBrand = $('.navbar-brand');
+const navbar = $('.navbar');
+const navToggler = $('.navbar-toggler');
+const welcomeSection = $('.welcome_section');
+const hamburgerBars = $('.fa-bars');
+const footer = $('footer');
+const brand1 = $('.brand1');
+const brand2 = $('.brand2');
+
+/*HELPER FNS*/
+const mobileView = () => {
+  return window.innerWidth < 900 ? true : false;
+};
+
+/* EVENT LISTENERS */
+$('.navbar-toggler').click(()=>{
+  const isOpen = $(collapsible).css('opacity') === '1';
+  if (isOpen){
+    closeNavbar(collapsible);
+  }
+  else{
+    openNavbar(collapsible);
+  }
+});
+
+$('.nav-link').click(()=>{
+  if(mobileView()){
+    closeNavbar(collapsible);
+  }
+});
+
+$('#top_of_page').click((e)=>{
+  let target = $(e.target);
+  //if the click is outside the navbar, and the navbar is on mobile and open, close it
+  if(!target.closest('.navbar').length && mobileView()) {
+    closeNavbar(collapsible);
+  } 
+});
+
+
 $(document).ready(function(){  
   checkWindowSize();
   
@@ -13,95 +56,78 @@ $(document).ready(function(){
     checkWindowSize();
     navbarChangeOnScroll();
   });
-  
 });
 
+
+/** Change the CSS of the navbar depending on where on the page we've scrolled to */
 function navbarChangeOnScroll(){
-  // change the navbar when you scroll 
   let stickySidebar = $('.creations').offset().top;
   stickySidebar= stickySidebar-95;
   if ($(window).scrollTop() > stickySidebar) {
-    $('.nav-link').css('color', '#e74c3c');
-    $('.nav-link').css('animation', 'font-color 30s infinite');
-    $('.nav-link').css('-webkit-animation', 'font-color 30s infinite');
-
-    // $('.mobile .nav-link').css('color', 'white');
-    // $('.mobile .nav-link').css('animation', 'none');
-    // $('.mobile .nav-link').css('-webkit-animation', 'none');
-      
-    $('.navbar-brand').css('color', '#e74c3c');
-    $('.navbar-brand').css('animation', 'font-color 30s infinite');
-    $('.navbar-brand').css('-webkit-animation', 'font-color 30s infinite');
-    $('.navbar').css('background-color', 'black');
-    $('.navbar').css('transition', ' background-color 1s');
-    // $('.navbar').css('border-color', '#e74c3c');
-    $('.navbar').css('transition', ' border-color 1s');
-    $('.navbar').css('animation', 'border-color 30s infinite');
-    $('.navbar').css('-webkit-animation', 'border-color 30s infinite');
-    $('.welcome_section').css('animation', 'none');
-    $('.welcome_section').css('-webkit-animation', 'none');
-
-    $('.custom-toggler.navbar-toggler ').css('border-color', '#e74c3c');
-    $('.custom-toggler.navbar-toggler ').css('animation', 'border-color 30s infinite');
-    $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'border-color 30s infinite');
-    $('.fa-bars').css('color', '#e74c3c');
-    $('.fa-bars').css('animation', 'font-color 30s infinite');
-    $('.fa-bars').css('-webkit-animation', 'font-color 30s infinite');
-    $('footer').css('animation', 'bg-color 30s infinite');
-    $('footer').css('-webkit-animation', 'bg-color 30s infinite');
-      
+    $(navLink).addClass('black-magic');
+    $(navLink).removeClass('rainbow');
+    $(navBrand).addClass('black-magic');
+    $(navBrand).removeClass('rainbow');
+    $(navbar).addClass('black-magic');
+    $(navbar).removeClass('rainbow');
+    $(hamburgerBars).addClass('black-magic');
+    $(hamburgerBars).removeClass('rainbow');
+    $(navToggler).addClass('justBorder');
+    $(welcomeSection).removeClass('justFont');    
+    $(footer).addClass('justBackground');
   }
   else {
-    $('.nav-link').css('color', 'black');
-    $('.nav-link').css('animation', 'none');
-    $('.nav-link').css('-webkit-animation', 'none');
-      
-    $('.navbar-brand').css('color', 'black');
-    $('.navbar-brand').css('animation', 'none');
-    $('.navbar-brand').css('-webkit-animation', 'none');
-    $('.navbar').css('transition', 'background-color 1s');
-    $('.navbar').css('background-color', '#e74c3c');
-    $('.navbar').css('animation', 'bg-color 30s infinite');
-    $('.navbar').css('-webkit-animation', 'bg-color 30s infinite');
-    // $('.navbar').css('border-color', 'transparent');
-    $('.navbar').css('transition', ' border-color 1s');
-    $('.welcome_section').css('animation', 'border-color 30s infinite');
-    $('.welcome_section').css('-webkit-animation', 'font-color 30s infinite');
- 
-    $('.custom-toggler.navbar-toggler ').css('border-color', 'black');
-    $('.custom-toggler.navbar-toggler ').css('animation', 'none');
-    $('.custom-toggler.navbar-toggler ').css('-webkit-animation', 'none');
-    $('.fa-bars').css('color', 'black');
-    $('.fa-bars').css('animation', 'none');
-    $('.fa-bars').css('-webkit-animation', 'none');
-    $('footer').css('animation', 'none');
-    $('footer').css('-webkit-animation', 'none');
+    $(navLink).removeClass('black-magic');
+    $(navLink).addClass('rainbow');
+    $(navBrand).removeClass('black-magic');
+    $(navBrand).addClass('rainbow');
+    $(navbar).removeClass('black-magic');
+    $(navbar).addClass('rainbow');
+    $(hamburgerBars).removeClass('black-magic');
+    $(hamburgerBars).addClass('rainbow');
+    $(navToggler).removeClass('justBorder');
+    $(welcomeSection).addClass('justFont');
+    $(footer).removeClass('justBackground');
   }  
 }
 
 function checkWindowSize(){
-  if (window.innerWidth < 900){
-    $('.collapse').css('opacity', '0');
-    $('.collapse').css('display', 'block');
-    $('.collapse').css('height','0');
-    $('.brand1').css('opacity',1);
-    $('.brand2').css('opacity',0);
-    $('.brand2').css('display','block');
-    $('.brand2').css('height','0');
-    $('.brand2').css('width','0');
-    $('.nav-link').fadeTo(1000,1);
+  if (mobileView()){
+    $(collapsible).css('opacity', '0');
+    $(collapsible).css('display', 'block');
+    $(collapsible).css('height','0');
+    $(brand1).css('opacity',1);
+    $(brand2).css('opacity',0);
+    $(brand2).css('display','block');
+    $(brand2).css('height','0');
+    $(brand2).css('width','0');
+    $(navLink).fadeTo(1000,1);
   }
   else{
-    $('.collapse').css('display', 'flex');
-    $('.collapse').css('opacity', '1');
-    $('.collapse').css('height','auto');
-    $('.brand1').css('opacity',0);
-    $('.brand2').css('opacity',1);
-    $('.brand2').css('height','auto');
-    $('.brand2').css('width','auto');
-    $('.nav-link').fadeTo(1000,1);
+    $(collapsible).css('display', 'flex');
+    $(collapsible).css('opacity', '1');
+    $(collapsible).css('height','auto');
+    $(brand1).css('opacity',0);
+    $(brand2).css('opacity',1);
+    $(brand2).css('height','auto');
+    $(brand2).css('width','auto');
+    $(navLink).fadeTo(1000,1);
   }
 }
+
+const closeNavbar = (collapsible) => {
+  $(collapsible).css('opacity','0');
+  $(collapsible).css('height','0');
+  $(collapsible).css('display', 'block');
+  $(navToggler).attr('aria-expanded', 'true');
+};
+
+const openNavbar = (collapsible) => {
+  $(collapsible).css('opacity','1');
+  $(collapsible).css('height','auto');
+  $(collapsible).css('display', 'block');
+};
+
 /**
  * Set focus to the first focusable child of an element.
  * @param node a jQuery element
@@ -113,22 +139,3 @@ function setFocusToFirstChild(elem) {
   // Get the first of the query result
   // focus on it
 }
-
-function toggleNavbar(){
-  const isOpen = $('.collapse').css('opacity') === '1';
-  const collapsible = $('.collapse');
-
-  if (isOpen){
-    // This makes a new object in memory
-    $('.collapse').css('opacity','0');
-    $('.collapse').css('height','0');
-    $('.collapse').css('display', 'block');
-    $('.navbar-toggler').attr('aria-expanded', 'true');
-  }
-  else{
-    $('.collapse').css('opacity','1');
-    $('.collapse').css('height','auto');
-    $('.collapse').css('display', 'block');
-  }
-}
-
